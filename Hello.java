5de98a7cc497f153314afbd882e2d39a008185cd
kimjaebeom <jabum2000@naver.com>
2020-06-01 15:25:48 +0900

    helloname()

--- Hello.java
+++ Hello.java
@@ -4,9 +4,16 @@ public class Hello {
 
 	public static void main(String[] args) {
 		hello();
+		helloName("Kacao");
+		helloName("Naver");
+		
 	}
 	public static void hello() {
 		System.out.println("Hello~");
+	}
+	
+	public static void helloName(String name) {
+		System.out.println("hello~"+ name);
 	}
 
 }
